8958e27fe32a8ad1fd0323f48a59dd2c0697365d
Wasim Abbas <abbas.wasim@gmail.com>
2021-06-16 11:54:59 +0100

    Change cputyptest.cmake

--- cmake/cputypetest.cmake
+++ cmake/cputypetest.cmake
@@ -15,98 +15,70 @@ set(cputypetest_code "
 //
 #ifdef _WIN32
     #ifdef _WIN64
-
-#undef x86_64
+        #undef x86_64
 x86_64
-
     #else
-
-#undef x86
+        #undef x86
 x86
-
     #endif
 #elif defined __APPLE__
     #include <TargetConditionals.h>
     #if TARGET_OS_IPHONE
         #if TARGET_CPU_X86
-
-#undef x86
+            #undef x86
 x86
 
         #elif TARGET_CPU_X86_64
-#undef x86_64
+            #undef x86_64
 x86_64
-
         #elif TARGET_CPU_ARM
-
-#undef armv7
+            #undef armv7
 armv7
-
         #elif TARGET_CPU_ARM64
-
-#undef armv8
+            #undef armv8
 armv8
-
         #else
             #error Unsupported cpu
         #endif
     #elif TARGET_OS_MAC
-
         #if defined __x86_64__
-
-#undef x86_64
+            #undef x86_64
 x86_64
-
         #elif defined __aarch64__
-
-#undef arm64
+            #undef arm64
 arm64
-
         #else
             #error Unsupported platform
         #endif
-
     #else
         #error Unsupported platform
     #endif
 #elif defined __linux
     #ifdef __ANDROID__
         #ifdef __i386__
-
-#undef x86
+            #undef x86
 x86
-
         #elif defined __arm__
-
-#undef armv7
+            #undef armv7
 armv7
-
         #elif defined __aarch64__
-
-#undef armv8
+            #undef armv8
 armv8
-
         #else
             #error Unsupported cpu
         #endif
     #else
         #ifdef __LP64__
-
-#undef x86_64
+            #undef x86_64
 x86_64
-
         #else
-
-#undef x86
+            #undef x86
 x86
-
         #endif
     #endif
 #elif defined __EMSCRIPTEN__
-
-#undef wasm
+    #undef wasm
 wasm
-
 #else
     #error Unsupported cpu
 #endif
